4a70854f38d7ad2624a42b7852f436a9eaee7b9f
Martinusbe <martinusbe@gmail.com>
2020-11-06 10:37:27 +0200

    surya: Adapt the exact values for lockscreen charging info
    Signed-off-by: Alexandru Scurtu <sasha.scurtu@gmail.com>
    Signed-off-by: Sherif Rahim <sherifrahim2001@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,6 +73,9 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # DT2W
 TARGET_TAP_TO_WAKE_NODE  := "/proc/tp_gesture"
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
