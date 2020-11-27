c43b7900fc5b6a18dfa1f5695db53592c1b9e4c8
daniml3 <danimoral1001@gmail.com>
2020-11-27 17:38:14 +0100

    surya: parts: Initial KCAL settings implementation
    * Added RGB, saturation and contrast settings
    * Added a preference for restoring default values
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,7 +20,9 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
+import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.utils.FileUtils;
@@ -33,10 +35,22 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         // Dirac
         DiracUtils.initialize(context);
+
+        // KCAL
+        FileUtils.writeLine(KcalUtils.KCAL_ENABLE_NODE,
+            sharedPrefs.getBoolean("kcal_enable", false) ? "1" : "0");
+
+        KcalUtils.writeConfigToNode(KcalUtils.KCAL_RGB_NODE, 1, sharedPrefs.getInt("red_slider", 256));
+        KcalUtils.writeConfigToNode(KcalUtils.KCAL_RGB_NODE, 2, sharedPrefs.getInt("green_slider", 256));
+        KcalUtils.writeConfigToNode(KcalUtils.KCAL_RGB_NODE, 3, sharedPrefs.getInt("blue_slider", 256));
+        KcalUtils.writeConfigToNode(KcalUtils.KCAL_SATURATION_NODE, 0, sharedPrefs.getInt("saturation_slider", 255));
+        KcalUtils.writeConfigToNode(KcalUtils.KCAL_CONTRAST_NODE, 0, sharedPrefs.getInt("contrast_slider", 255));
 
         // Thermal Profiles
         ThermalUtils.startService(context);
