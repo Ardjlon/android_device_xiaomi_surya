e6caf9f3c306477640f6d214c5753314198e0da0
Genkzsz11 <genkzsz11@nusantaradevs.org>
2021-04-28 02:07:19 +0700

    surya: Nad ify
    Signed-off-by: Genkzsz11 <genkzsz11@nusantaradevs.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/wave_surya.mk
+    $(LOCAL_DIR)/nad_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    wave_surya-user \
-    wave_surya-userdebug \
-    wave_surya-eng
+    nad_surya-user \
+    nad_surya-userdebug \
+    nad_surya-eng
