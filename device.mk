f1c39e343a03100ac89e226cdfaadf31c71ce013
changyan <changyan@google.com>
2021-06-08 14:00:33 +0800

    surya: Add qti-telephony-hidl-wrapper-prd
    Bug: 188781413
    Change-Id: I4eeb211afab03198d07a226a0854b1c8bc39cd4f

--- device.mk
+++ device.mk
@@ -554,6 +554,8 @@ PRODUCT_PACKAGES += \
     extphonelib_product.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
+    qti-telephony-hidl-wrapper-prd \
+    qti_telephony_hidl_wrapper-prd.xml \
     qti-telephony-utils \
     qti-telephony-utils-prd \
     qti_telephony_utils.xml \
