93c941ecbe7ef8a7471653ccf9ea23a59a1266eb
Genkzsz11 <Genkzsz11@gmail.com>
2021-06-29 19:37:04 +0700

    surya: aosp_base_telephony to full_base_telephony
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- nad_surya.mk
+++ nad_surya.mk
@@ -16,7 +16,7 @@
 
 # Inherit framework first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
