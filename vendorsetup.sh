8c5a954a36d85a4895ec6d6bc607bcd6c2d1ac4d
Ardjlon <carlosardany@gmail.com>
2021-07-14 03:58:44 +0000

    surya: Disable realme partitions
    playing with git

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,3 +5,7 @@ git clone https://github.com/stormbreaker-project/kernel_xiaomi_surya  -b rebase
 git clone https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-13 --depth=1
 touch prebuilts/clang/host/linux-x86/clang-13/AndroidVersion.txt
 echo "13" >> prebuilts/clang/host/linux-x86/clang-13/AndroidVersion.txt
+
+# Disable Realme partitions
+git -C build/tools fetch https://github.com/Evolution-X/build elle 75895232a2b045431589bcb31ed808cc86ebffa4
+git -C build/tools revert 75895232a2b045431589bcb31ed808cc86ebffa4 --no-edit
