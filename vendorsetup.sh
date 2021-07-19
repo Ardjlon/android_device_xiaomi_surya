32740e99e84641e8b6f3ca3b1d5478483edddf6c
Ardjlon <carlosardany@gmail.com>
2021-07-19 14:07:02 +0000

    surya: vendorsetup: add firmware repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,3 +9,6 @@ echo "13" >> prebuilts/clang/host/linux-x86/clang-13/AndroidVersion.txt
 # Disable Realme partitions
 git -C build/tools fetch https://github.com/Evolution-X/build elle 75895232a2b045431589bcb31ed808cc86ebffa4
 git -C build/tools revert 75895232a2b045431589bcb31ed808cc86ebffa4 --no-edit
+
+# Firmware
+git clone https://gitlab.com/Ardjlon/firmware_xiaomi_surya firmware/xiaomi/surya
