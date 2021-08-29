66ab35a4b8705ee04c28df56b1607bffa0705955
Ardjlon <carlosardany@gmail.com>
2021-08-29 22:51:48 +0000

    surya: improve device properties *This fixes Netflix and Prime Video

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -126,9 +126,37 @@ void vendor_load_properties()
     if (GetProperty("ro.boot.hwname", "") == "karna") { // POCO X3 (India)
         set_device_props("POCO", "karna", "M2007J20CI", "karna_in");
         property_override("ro.product.mod_device", "surya_in_global");
+        property_override("ro.product.model", "M2007J20CI");
+        property_override("ro.product.system.model", "qssi system image for arm64");
+        property_override("ro.product.vendor.model", "M2007J20CI");
+        property_override("ro.product.product.model", "qssi system image for arm64");
+        property_override("ro.product.odm.model", "M2007J20CI");
+        property_override("ro.product.system_ext.model", "qssi system image for arm64");
+        property_override("ro.product.build.fingerprint", "qti/qssi/qssi:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.system_ext.build.fingerprint", "qti/qssi/qssi:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.system.build.fingerprint", "qti/qssi/qssi:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.vendor.build.fingerprint", "POCO/karna_global/karna:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.bootimage.build.fingerprint", "POCO/karna_global/karna:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.odm.build.fingerprint", "POCO/karna_global/karna:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.build.fingerprint", "POCO/karna_global/karna:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.build.description", "karna_global-user 11 RKQ1.200826.002 V12.5.1.0.RJGMIXM release-keys");
     } else { // POCO X3 NFC
-        set_device_props("POCO", "surya", "M2007J20CG", "surya_global");
+        set_device_props("POCO", "surya", "M2007J20CI", "surya_global");
         property_override("ro.product.mod_device", "surya_global");
+        property_override("ro.product.model", "M2007J20CI");
+        property_override("ro.product.system.model", "qssi system image for arm64");
+        property_override("ro.product.vendor.model", "M2007J20CI");
+        property_override("ro.product.product.model", "qssi system image for arm64");
+        property_override("ro.product.odm.model", "M2007J20CI");
+        property_override("ro.product.system_ext.model", "qssi system image for arm64");
+        property_override("ro.product.build.fingerprint", "qti/qssi/qssi:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.system_ext.build.fingerprint", "qti/qssi/qssi:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.system.build.fingerprint", "qti/qssi/qssi:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.vendor.build.fingerprint", "POCO/surya_global/surya:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.bootimage.build.fingerprint", "POCO/surya_global/surya:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.odm.build.fingerprint", "POCO/surya_global/surya:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.build.fingerprint", "POCO/surya_global/surya:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys");
+        property_override("ro.build.description", "surya_global-user 11 RKQ1.200826.002 V12.5.1.0.RJGMIXM release-keys");
     }
 
     load_dalvik_properties();
