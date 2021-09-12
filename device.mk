af47e3f8f40e08c4d0db69ae5927f894d3787514
Ardjlon <carlosardany@gmail.com>
2021-09-12 18:43:47 +0000

    surya: add doze mode

--- device.mk
+++ device.mk
@@ -99,6 +99,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
     $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # Camera
 PRODUCT_PACKAGES += \
     libshim_megvii
