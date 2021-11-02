bf594b9dd30f03d1c793b7708a780ddfb6c57977
TheStrechh <arriagagarciajc@gmail.com>
2021-11-02 10:09:13 -0600

    surya: Drop LiveDisplay
    This reverts commit b1b6f65a063edf70d1524506e22f9b0f65eedd6a.

--- device.mk
+++ device.mk
@@ -245,10 +245,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.surya
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
