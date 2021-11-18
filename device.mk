3418f18ede7d5261db15233959aa6a44558ba4b0
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-11-18 08:05:04 -0400

    surya: rro_ovelays: Move CarrierConfigOverlay to rro_overlay
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: I0caa033985aedcefdb1ad14b118b8c3e413f2672

--- device.mk
+++ device.mk
@@ -353,9 +353,6 @@ PRODUCT_COPY_FILES += \
 # Overlay
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
-
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
@@ -412,6 +409,10 @@ PRODUCT_PACKAGES += \
     libril \
     librilutils \
     librmnetctl
+
+# RIL Overlay
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlay
 
 # Recovery
 PRODUCT_PACKAGES += \
