1039e44b7de137b09e67a7a22027224db40ec893
ᴄᴀʀʟᴏꜱ ᴀʀʀɪᴀɢᴀ <37526095+TheStrechh@users.noreply.github.com>
2021-12-02 20:49:21 -0600

    surya: Drop flag CONFIG_MSM_PERFORMANCE this CAF

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,7 +115,6 @@ BOARD_KERNEL_CMDLINE += kpti=off cgroup.memory=nokmem,nosocket
 
 TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
-TARGET_KERNEL_ADDITIONAL_FLAGS += CONFIG_MSM_PERFORMANCE=n
 
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_USE_LATEST_CLANG := true
