ad44c414463662b6386becb3b3e65bef10ba838c
Ardjlon <carlosardany@gmail.com>
2022-01-16 21:21:04 +0000

    surya: test

--- device.mk
+++ device.mk
@@ -428,6 +428,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti \
+    android.hardware.power@1.0-impl \
     android.hardware.power-service.xiaomi-libperfmgr
 
 # Protobuf
