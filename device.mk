440d73953d3694f8d7782795e52764ed3191be10
Ardjlon <carlosardany@gmail.com>
2022-02-18 16:06:22 +0000

    surya: drop exclusive 2016 configs

--- device.mk
+++ device.mk
@@ -429,7 +429,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Pixel features
 PRODUCT_COPY_FILES += \
-    vendor/gms/product/blobs/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml \
     vendor/gms/product/blobs/etc/sysconfig/game_overlay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/game_overlay.xml
 
 # Platform
