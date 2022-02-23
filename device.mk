775eeec3730a12735a199338dae412807b627f5f
Ardjlon <carlosardany@gmail.com>
2022-02-23 16:12:49 -0600

    surya: Build android.system.keystore2 * Sepolicy: label package

--- device.mk
+++ device.mk
@@ -337,6 +337,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1 \
     android.hardware.keymaster@4.1.vendor
 
+# Keystore
+PRODUCT_PACKAGES += \
+    android.system.keystore2
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.surya
