26834778a39f3b3a6ff902d8dc6aac465934b5e5
Ardjlon <carlosardany@gmail.com>
2022-03-27 20:38:04 +0000

    surya: Switch to QuickSilver Kernel * Update clang version to 15

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,3 @@
-# Kernel
-git clone git@github.com:stormbreaker-project/kernel_xiaomi_surya.git -b staging/main kernel/xiaomi/surya
-
 # Disable Realme partitions
 git -C build/tools fetch https://github.com/Evolution-X/build elle 75895232a2b045431589bcb31ed808cc86ebffa4
 git -C build/tools revert 75895232a2b045431589bcb31ed808cc86ebffa4 --no-edit
@@ -17,10 +14,6 @@ git -C hardware/qcom-caf/sm8150/display fetch pixel && git -C hardware/qcom-caf/
 
 # Google Camera
 git clone git@github.com:Ardjlon/vendor_GoogleCamera.git vendor/GoogleCamera
-
-# EVA GCC
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 prebuilts/gcc/linux-x86/aarch64/aarch64-elf
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm prebuilts/gcc/linux-x86/arm/arm-eabi
 
 # PocoParts
 git clone git@github.com:Ardjlon/vendor_PocoParts.git -b elle vendor/PocoParts
