3745d27d7cdc5912b1af8fc061fab07c6caa1084
Ardjlon <carlosardany@gmail.com>
2022-04-08 16:48:55 +0000

    surya: corvus: Add maintainer

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,6 +14,7 @@ BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
 BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 
 # Inherit from proprietary configuration
 include vendor/xiaomi/surya/BoardConfigVendor.mk
