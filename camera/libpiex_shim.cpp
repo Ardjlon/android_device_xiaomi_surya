d6d1be0ee20632b83bdff6711ebce3abdf182f95
daniml3 <daniel@danielml.dev>
2022-08-19 13:56:19 +0200

    surya: Add GetPreviewImageData shim for camera
    Change-Id: Iad9d37b992775b6d7b9b52ed4637c70fd13ede7f
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- camera/libpiex_shim.cpp
+++ camera/libpiex_shim.cpp
@@ -8,9 +8,16 @@
 
 namespace piex {
 
-Error GetPreviewImageData(StreamInterface* data,
-                          PreviewImageData* preview_image_data) {
-    return GetPreviewImageData(data, preview_image_data, nullptr);
+using image_type_recognition::RawImageTypes;
+
+extern "C" void
+    _ZN4piex19GetPreviewImageDataEPNS_15StreamInterfaceEPNS_16PreviewImageDataEPNS_22image_type_recognition13RawImageTypesE(
+    StreamInterface* data, PreviewImageData* preview_image_data, RawImageTypes* output_type);
+
+
+extern "C" void _ZN4piex19GetPreviewImageDataEPNS_15StreamInterfaceEPNS_16PreviewImageDataE(
+    StreamInterface* data, PreviewImageData* preview_image_data) {
+	return _ZN4piex19GetPreviewImageDataEPNS_15StreamInterfaceEPNS_16PreviewImageDataEPNS_22image_type_recognition13RawImageTypesE(data, preview_image_data, nullptr);
 }
 
-} // namespace piex
+} // namespace android
