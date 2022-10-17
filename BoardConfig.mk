32a933e81d6bda3352c0d3b76743c6c7beedf74b
Ardjlon <carlosardany@gmail.com>
2022-10-17 21:46:49 +0000

    surya: Update kernel defconfig * Switch to Deluxe Kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,7 +92,7 @@ BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 
 TARGET_KERNEL_APPEND_DTB := false
-TARGET_KERNEL_CONFIG := surya_defconfig
+TARGET_KERNEL_CONFIG := vendor/surya-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom
