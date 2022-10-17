0a67e75d7b2cf52ddf3ae23696143f5c4b90f914
Ardjlon <carlosardany@gmail.com>
2022-10-17 21:56:53 +0000

    surya: Add PATH for Clang-16

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := 16
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-16
 
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
