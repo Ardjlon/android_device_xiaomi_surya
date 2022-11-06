537f35b5b9c856c3dec6d4a0239c2d244374c59e
Ardjlon <carlosardany@gmail.com>
2022-11-06 17:50:23 +0000

    surya: parts: Update nodes for kprofiles

--- parts/src/org/lineageos/settings/kprofiles/Constants.java
+++ parts/src/org/lineageos/settings/kprofiles/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
     public static final String KEY_KPROFILES_AUTO = "pref_kprofiles_auto";
     public static final String KPROFILES_AUTO_NODE = "/sys/module/kprofiles/parameters/auto_kprofiles";
     public static final String KEY_KPROFILES_MODES = "pref_kprofiles_modes";
-    public static final String KPROFILES_MODES_NODE = "/sys/module/kprofiles/parameters/mode";
+    public static final String KPROFILES_MODES_NODE = "/sys/module/kprofiles/parameters/kp_mode";
     public static final String KPROFILES_MODES_INFO = "pref_kprofiles_modes_info";
-} 
+}
